8ff9ba80a420aaec95009de583b2b474baa49600
joelmatiassilva <josilva@mailexternos.com.ar>
2019-12-17 18:10:14 -0300

    new commit

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+    <React.Fragment>
+        <h2 className="error__container--title">404</h2>
+        <p>Página no encontrada</p>
+    </React.Fragment>
+  )
+  
+  export default NotFound;
