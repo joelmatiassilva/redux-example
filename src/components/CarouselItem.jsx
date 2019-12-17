b351335b1071de11be0314e0d975e4eb8724435b
joelmatiassilva <josilva@mailexternos.com.ar>
2019-12-17 18:29:24 -0300

    sin bug

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -7,7 +7,7 @@ import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
 const CarouselItem = (props) =>{ 
-  const { id, cover, title, year, contentRating, duration } = props;
+  const { id, cover, title, year, contentRating, duration, isList } = props;
 
   const handleSetFavorite = () => {
     props.setFavorite({
@@ -25,8 +25,13 @@ const CarouselItem = (props) =>{
   <div className="carousel-item__details">
     <div>
       <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
-      <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" onClick={ handleSetFavorite } />
-      <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" onClick={ () => { handleDeleteFavorite(id) }} />
+      
+      {
+        isList ? <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" onClick={ () => { handleDeleteFavorite(id) }} /> :
+        <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" onClick={ handleSetFavorite } />
+      }
+
+
     </div>
     <p className="carousel-item__details--title">{title}</p>
     <p className="carousel-item__details--subtitle">
